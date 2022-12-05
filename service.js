38b6de83b52e6643d83b3117bd427857ab4e2466
ImmDz <johnbessmerntyjj@gmail.com>
2022-12-05 15:30:37 +0300

    Added files

---
+++ service.js
@@ -0,0 +1,31 @@
+export const storage = {
+    getTasks(defaultTasksList = []) {
+        const tasks = localStorage.getItem("tasks");
+
+        return tasks ? JSON.parse(tasks) : defaultTasksList;
+    },
+    setTasks(tasks) {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    },
+    getTheme(defaultTheme = true) {
+        const state = localStorage.getItem("state");
+        return state ? JSON.parse(state) : defaultTheme;
+    },
+    setTheme(state) {
+        localStorage.setItem("state", JSON.stringify(state));
+    },
+    getFilter(defaultFilter = "") {
+        const filter = sessionStorage.getItem("filter");
+        return filter ? JSON.parse(filter) : defaultFilter;
+    },
+    setFilter(filter) {
+        sessionStorage.setItem("filter", JSON.stringify(filter));
+    },
+    getList(defaultTasksList = this.getTasks()) {
+        const list = sessionStorage.getItem("list");
+        return list ?  JSON.parse(list) : defaultTasksList;
+    },
+    setList(list) {
+        sessionStorage.setItem("list", JSON.stringify(list));
+    }
+};
